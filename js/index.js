2c6288c7607d47ce11f7d625415648445b04cb90
zhougogo <1290612478@qq.com>
2017-05-23 10:25:47 +0800

    add
    啊大大大

--- js/index.js
+++ js/index.js
@@ -2,71 +2,140 @@
 
 //第一页loading
 
-//count();
+count();
 
 function count(){
 	
 	var num = 0;
 	
-	$('.page1 .loadText span').html(num+'%');
+	var random = 0;
+	
+	$('.page1 .loadText span').html(random+'%');
 	
 	var timer = setInterval(function(){
-	
-		var random = Math.floor(Math.random()*20);
 		
-		num+=random;
+		num++;
 		
-		if(num > 99){
+		RandomNum((num-1)*20, num*20);
+		
+		//取
+		function RandomNum(Min, Max) {
 			
-			clearInterval(timer);
+	      var Range = Max - Min;
+	      
+	      var Rand = Math.random();
+	      
+	      if(Math.round(Rand * Range)==0){
+	      	
+	        return Min + 1;
+	        
+	      }else if(Math.round(Rand * Max)==Max)
+	      
+	      {
+	      	
+	        index++;
+	        
+	        return Max - 1;
+	        
+	      }else{
+	      	
+	        random = Min + Math.round(Rand * Range) - 1;
+	        
+	        return random;
+	        
+	      }
+	      
+	 }
+		
+		
 			
-			num = 99;
-			
-			$('.page2').show().siblings().hide();
-			
-			$('.page2').animate({'top':'-200%'},3000);
-		}
+		if(num > 5){
+				
+				clearInterval(timer);
+				
+				
+				
+				$('.page2').show().siblings().hide();
+				
+				$('.page2').animate({'top':'-200%'},3000);
+			}
 		
 		
 		
-		$('.page1 .loadText span').html(num+'%');
+			$('.page1 .loadText span').html(random+'%');
 		
 
 		
 //		console.log(random);
 		
-	},500)
+	},1000)
 }
 
 
 //海报生成中
 
 function count2(){
+
+	$('.page33').show();
 	
+	var random = 0;
+
 	var num = 0;
+	$('.page5').addClass('pageBegin');
 	
-	$('.page5 .loadText span').html(num+'%');
+	$('.page5 .loadText span').html(random+'%');
 	
 	var timer = setInterval(function(){
-	
-		var random = Math.floor(Math.random()*20);
+		num++;
 		
-		num+=random;
+		RandomNum((num-1)*20, num*20);
 		
-		if(num > 99){
+		//取
+		function RandomNum(Min, Max) {
+			
+	      var Range = Max - Min;
+	      
+	      var Rand = Math.random();
+	      
+	      if(Math.round(Rand * Range)==0){
+	      	
+	        return Min + 1;
+	        
+	      }else if(Math.round(Rand * Max)==Max)
+	      
+	      {
+	      	
+	        index++;
+	        
+	        return Max - 1;
+	        
+	      }else{
+	      	
+	        random = Min + Math.round(Rand * Range) - 1;
+	        
+	        return random;
+	        
+	      }
+	      
+	 }
+
+		
+		if(num > 5){
 			
 			clearInterval(timer);
 			
-			num = 99;
-			
-			$('.page6').show().siblings().hide();
 			
 
+			clip();
+			
+			$('.page6').show().siblings().hide();
+				
+			
 		}
 		
 		
 		
-		$('.page5 .loadText span').html(num+'%');
+		$('.page5 .loadText span').html(random+'%');
 		
 
 		
@@ -74,30 +143,67 @@ function count2(){
 		
 	},500)
 }
-
-//生成海报
+	//生成海报
 	$('.footerBtnRight').click(function(){
-	
-//		for(var i = 0;i < $('.text').length;i++){
-//			
-//			$('.spanText'+i).html($('.text'+i).val()) ;
-//			
-//		}
-//		
-//			$('.page33').show().siblings().hide();
+		
+		// console.log(123);
+		//高冷模式和话唠模式
+		for(var i = 0;i < $('.personInfo .text').length+1;i++){
+			
+			$('.personInfo .spanText'+i).html($('.personInfo .text'+i).val()) ;
+			
+		}
+		for(var i = 0;i < $('.personInfo2 .text').length+1;i++){
+			
+			$('.personInfo2 .spanText'+i).html($('.personInfo2 .text'+i).val()) ;
+			
+		}
 
-		$('.page5').show().siblings().hide();
 		
-			count2();
+		$('.page5').show();
+		$('.face').hide();
+
+		//截取图片
 		
-		})
+
+
+
+		// $('.page5').show().siblings().hide();
+		
+		count2();
+		
+	})
+
 //话唠模式切换
 
 $('.sayBtm img').click(function(){
 	
-	$('.page3').show().siblings().hide();
+	// console.log(123);
+
+	$('.face').show().siblings().hide();
+
+	$('.personInfo').show();
+
+	$('.personInfo2').hide();
+
+
 	
+});
+
+//高冷模式
+
+$('.lBtm img').click(function(){
+
+	$('.face').show().siblings().hide();
+
+	$('.personInfo2').show();
+
+	$('.personInfo').hide();
+
+
 })
+
+
 
 //重做一次
 
@@ -123,8 +229,57 @@ $('.page3').height(height);
 
 $('.page3 .page3Container').height(height)
 
-//console.log(height);
 
+
+//截屏
+
+function clip(){
+
+	var pa = document.getElementById('pa');
+
+	var canvas2 = document.getElementById('canvas2');
+
+	html2canvas(pa).then(function(canvas) {
+
+	         canvas2.appendChild(canvas);
+
+	     })	
+	}
+
+
+// 加白条
+space();
+
+function space(){
+
+	var space = $('.spanText');
+
+for(var i = 0;i < space.length;i++){
+		
+		var reg = /[a-zA-Z0-9]+/;
+		
+		var eText = $('.spanText').eq(i).text();
+		
+		for(var j = 0;j < eText.length;j++){
+			
+			console.log(eText.substring(j,j+1))
+			
+			
+			
+		}
+
+
+		var len = $('.spanText').eq(i).text().length;
+
+		$('.spanText').eq(i).css('background-image','url("img/space/space'+len+'.png")');
+		
+	}
+
+
+}
+
+
+		
 
 
 
